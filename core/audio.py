dc8ea4dd4c43098891b823aec9719d1be3befa2a
kyb3r <abdurraqeeb53@gmail.com>
2019-06-05 01:03:46 +1000

    black code style

--- core/audio.py
+++ core/audio.py
@@ -2,21 +2,23 @@ import pyttsx3
 import json
 
 
-def generate_audio(comment_text):
+def generate_audio(title, comment_text):
     engine = pyttsx3.init()
 
-    engine.setProperty('rate', 180)
-    voices = engine.getProperty('voices')
+    engine.setProperty("rate", 180)
+    voices = engine.getProperty("voices")
 
     try:
-        voice = [v for v in voices if v.name == 'Daniel'][0]
+        voice = [v for v in voices if v.name == "Daniel"][0]
     except IndexError:
         pass
     else:
-        engine.setProperty('voice', voice.id)
+        engine.setProperty("voice", voice.id)
+
+    engine.save_to_file(title, "dump/title.mp3")
 
     for id, text in comment_text.items():
-        engine.save_to_file(text, f'dump/audio-{id}.mp3')
-        print('Generating audio for comment', id)
+        engine.save_to_file(text, f"dump/audio-{id}.mp3")
+        print("Generating audio for comment", id)
 
     engine.runAndWait()
