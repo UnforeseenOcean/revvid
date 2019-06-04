3c46c9d6219c4fb870688e7da9eaecbd5846fccb
kyb3r <abdurraqeeb53@gmail.com>
2019-06-05 07:50:32 +1000

    Better outro text

--- app.py
+++ app.py
@@ -36,7 +36,11 @@ else:
 
 if __name__ == "__main__":
     comment_text = fetch_screenshots(submission, night_mode=True, limit=3)
-    title_text = 'r slash ask reddit... ' + submission.title # lol
-    generate_audio(title_text, comment_text, end='Thanks for watching, make sure to like and subscribe since I am not a bot lol.')
+    title_text = "r slash ask reddit... " + submission.title  # lol
+    generate_audio(
+        title_text,
+        comment_text,
+        end="Thanks for watching mate, make sure to smash that star button for more high quality content.",
+    )
     composite_video()
     print("Done.")
